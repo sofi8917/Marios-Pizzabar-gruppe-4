18853d6d8dd97727bf302874843bec2d631e6832
sofi8917 <sona0001@stud.kea.dk>
2024-10-29 09:37:44 +0100

    hello

---
+++ src/Pizza.java
@@ -0,0 +1,40 @@
+public class Pizza {
+    int nummerPizza;
+    String navnPizza;
+    int prisPizza;
+
+    public Pizza(int nummerPizza, String navnPizza, int prisPizza){
+        this.nummerPizza = nummerPizza;
+        this.navnPizza = navnPizza;
+        this.prisPizza = prisPizza;
+    }
+
+    public void setNummerPizza(int nummerPizza){
+        this.nummerPizza = nummerPizza;
+    }
+
+    public void setNavnPizza(String navnPizza){
+        this.navnPizza = navnPizza;
+    }
+
+    public void setPrisPizza(int prisPizza) {
+        this.prisPizza = prisPizza;
+    }
+
+    public int getNummerPizza(){
+        return nummerPizza;
+    }
+
+    public String getNavnPizza(){
+        return navnPizza;
+    }
+
+    public int getPrisPizza() {
+        return prisPizza;
+    }
+
+    public String toString(){
+        return nummerPizza + ". " + navnPizza + ": " + prisPizza +" kr.";
+    }
+
+}
